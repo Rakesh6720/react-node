c33015226aa4ada3fbb70ebe52507560f5eedb9b
Rakesh6720 <rbaruah@gmail.com>
2022-01-07 09:36:57 -0600

    add MainHeader to App.js

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -6,12 +6,12 @@ import "./MainNavigation.css";
 const MainNavigation = (props) => {
   return (
     <MainHeader>
-      <button>
+      <button className="main-navigation__menu-btn">
         <span />
         <span />
         <span />
       </button>
-      <h1 className="main-navigation">
+      <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
       <nav>...</nav>
