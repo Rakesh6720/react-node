32d10069f1e6e692e92085281b9bb357d6916c52
Rakesh6720 <rbaruah@gmail.com>
2022-01-07 09:28:54 -0600

    add shared navigation components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -12,7 +12,6 @@ const UserItem = (props) => {
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div>
-
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
